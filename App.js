c32babe31c55c1a4bfddcc54e1790f3bc1dbe06b
sdalal <sarojdalal@hotmail.com>
2022-04-27 13:15:42 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+﻿import React, { Component } from 'react';
+import { Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
+import Home from './components/Home';
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+render() {
+    return (
+            <div>
+                <BrowserRouter>
+                    <Switch>
+
+                        {/* Otherwise, render the Landing component */}
+                        <Route path="/" component={Home} />
+
+                    </Switch>
+                </BrowserRouter>
+            </div>
+    );
+}
+}
+
+export default App;
+
